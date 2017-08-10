1668ecde06b53da3ca5d0dd97ec185c6d5bfc86b
Tarmin Sidharta <tsidharta@gmail.com>
2017-08-09 20:26:14 -0700

    delete message.js

--- views/text.js
+++ views/text.js
@@ -1,19 +1,8 @@
 
 
-var Nexmo = require('nexmo');
-
-var nexmo = new Nexmo({
-    apiKey: 114585e8
-    apiSecret: 00ba2e565ebb5350
-    applicationId: APP_ID,
-    privateKey: PRIVATE_KEY_PATH,
-  }, options);
-
-
-
-curl -X "POST" "https://rest.nexmo.com/sms/json" \
-  -d "from=Nexmo" \
-  -d "text=A text message sent using the Nexmo SMS API" \
-  -d "to=TO_NUMBER" \
-  -d "api_key=API_KEY" \
-  -d "api_secret=API_SECRET"
+var TMClient = require('textmagic-rest-client');
+  
+var c = new TMClient('tarminsidharta', '2gifxFVdkChHlkFBHsMNctS9ZeI8ho');
+c.Messages.send({text: 'Your table is ready..John', phones:'14803885693'}, function(err, res){
+    console.log('Messages.send()', err, res);
+});
