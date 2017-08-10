26c85f07c141d4b6b922311d2a2be2576a5c8f71
John Silkey <jpsilkjr86@gmail.com>
2017-08-10 09:09:46 -0700

    added post request api route to catch customer information, setting some default values

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -10,4 +10,30 @@ module.exports = (app, passport) => {
 		successRedirect: '/dashboard',
 		failureRedirect: '/signin'
 	}));
+	// route for adding new customers
+	app.post('/customer/add', (req, res) => {
+		if (!req.user) {
+			res.redirect('/signin');
+			// display message saying that form submission failed, plz sign in
+		}
+		else {
+			// instantiates locally scoped customer object, values determined by 
+			// req.body, req.user and some other defaults.
+			const customer = {
+				party_name: req.body.party_name,
+				party_size: req.body.party_size,
+				phone_number: req.body.phone_number,
+				email: req.body.email,
+				occasion: req.body.occasion,
+				first_name: req.body.first_name,
+				last_name: req.body.last_name,
+				restaurant_id: req.user.id,
+				active: true,
+				arrived_table: false,
+				alerted_sms: false
+			};
+			console.log(customer);
+			res.json(customer);
+		}
+	});
 };
