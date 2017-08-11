d506dd39a75d25e4cc4bace5b1724c712078e757
John Silkey <jpsilkjr86@gmail.com>
2017-08-10 19:51:10 -0700

    added return promise to try and fix issue with new user being unable to see their newly added customers until logging off and signing back in

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -17,40 +17,45 @@ module.exports = (app, db, passport) => {
 			// display message saying that form submission failed, plz sign in
 		}
 		else {
-			console.log('User Data:');
-			console.log(req.user);
-			// if occasion is array, joins as string
+			// instantiates locally scoped variables to be used in new customer object
+			let restaurant_id;
 			let occasion;
+			// if occasion is array, joins as string
 			if (Array.isArray(req.body.occasion)) {
 				occasion = req.body.occasion.join();
 			} else {
 				occasion = req.body.occasion;
 			}
-			// instantiates locally scoped customer object, values determined by 
-			// req.body, req.user and some other defaults.
-			const customer = {
-				party_name: req.body.party_name,
-				party_size: req.body.party_size,
-				phone_number: req.body.phone_number,
-				email: req.body.email,
-				occasion: occasion,
-				first_name: req.body.first_name,
-				last_name: req.body.last_name,
-				restaurant_id: req.user.id,
-				active: true,
-				arrived_table: false,
-				alerted_sms: false
-			};
-			console.log(customer);
-			// attempts to add to database
-			db.CustTable.create(customer).then(result => {
+			// gets restaurant_id by querying user data
+			db.TestTable.findOne({where: {email: req.user.email}}).then(result => {
+				// saves restaurant id as result.id (of user)
+				restaurant_id = result.id;
+				// instantiates locally scoped customer object, values determined by 
+				// req.body, req.user and some other defaults.
+				const customer = {
+					party_name: req.body.party_name,
+					party_size: req.body.party_size,
+					phone_number: req.body.phone_number,
+					email: req.body.email,
+					occasion: occasion,
+					first_name: req.body.first_name,
+					last_name: req.body.last_name,
+					restaurant_id: restaurant_id,
+					active: true,
+					arrived_table: false,
+					alerted_sms: false
+				};
+				console.log(customer);
+				// attempts to add to database
+				 return db.CustTable.create(customer);
+			}).then(result => {
 				console.log(result);
 				// set session success message
-				res.redirect('/dashboard');
+				res.redirect('/');
 			}).catch(err => {
-				console.log(err);
-				// set session error
-				res.redirect('/dashboard');
+				console.log('FAILED TO GET USER DATA WHEN ADDING CUSTOMER');
+				// set session error message
+				res.redirect('/');
 			});
 		}
 	});
