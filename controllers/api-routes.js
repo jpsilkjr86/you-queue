411dd1bac381d9bdefff7330e29e48f347db40a7
Tarmin Sidharta <tsidharta@gmail.com>
2017-08-11 22:56:10 -0700

    text message button make functional

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,3 +1,6 @@
+// import sms client
+const youQueueSMS = require('../custom_modules/youqueue-sms.js');
+
 // exports as a function which takes in express app and passport parameters
 module.exports = (app, db, passport) => {
 	// route for signing up new users. authenticates with passport local strategy 'local-signup'
@@ -10,6 +13,7 @@ module.exports = (app, db, passport) => {
 		successRedirect: '/dashboard',
 		failureRedirect: '/signin'
 	}));
+
 	// route for adding new customers
 	app.post('/customer/add', (req, res) => {
 		if (!req.user) {
@@ -53,9 +57,56 @@ module.exports = (app, db, passport) => {
 				// set session success message
 				res.redirect('/');
 			}).catch(err => {
-				console.log('FAILED TO GET USER DATA WHEN ADDING CUSTOMER');
+				console.log('FAILED TO ADD CUSTOMER');
+				console.log(err);
 				// set session error message
 				res.redirect('/');
+			});
+		}
+	});
+
+    // route for alerting sms to customer
+	app.post('/customer/alerted', (req, res) => {
+		if (!req.user) {
+			res.redirect('/signin');
+			// display message saying that form submission failed, plz sign in
+		}
+		else {
+			//from req.body.id information, runs query to get phone number 
+			db.CustTable.findOne({where: {id: req.body.id}}).then(result => {
+            //then do text magic function
+            	let phone_number = '1' + result.phone_number;
+            	let message = 'You-Queue Alert for ' + result.party_name + ': Your table is ready!'
+							+ ' Please come at your earliest convenience.'
+							+ ' Enjoy your meal! (AUTO-SMS:Do not reply!)';
+
+				youQueueSMS.Messages.send({text: message, phones: phone_number}, (err, res_sms) => {
+					//console.log('Messages.send()', err, res);
+					if (err) {
+						console.log(err);
+						res.send('SMS message failed');
+					} else {
+						console.log('SMS message successfully sent');
+						console.log(res_sms);
+						db.CustTable.update( { 
+							alerted_sms: req.body.alerted_sms 
+						} , {
+							where: {
+								id: req.body.id
+							}
+						}).then(updateResult => {
+							console.log("CUSTOMER HAS BEEN ALERTED BY TEXT MESSAGE");
+							res.send("CUSTOMER HAS BEEN ALERTED BY TEXT MESSAGE");
+						}).catch(err => {
+							console.log('FAILED TO ALERT CUSTOMER BY TEXT MESSAGE');
+							// set session error message
+							res.redirect('/');
+						});
+
+					}
+						
+				});
+
 			});
 		}
 	});
@@ -69,8 +120,8 @@ module.exports = (app, db, passport) => {
 		}
 		else {
 			db.CustTable.update( { active: req.body.active } , {where: {id: req.body.id}}).then(result => {
-				console.log("CUSTOMER ACTIVE STATUS SUCCESSFULLY UPDATED");
-				res.send("CUSTOMER ACTIVE STATUS SUCCESSFULLY UPDATED");
+				console.log('CUSTOMER ACTIVE STATUS SUCCESSFULLY UPDATED');
+				res.send('CUSTOMER ACTIVE STATUS SUCCESSFULLY UPDATED');
 			}).catch(err => {
 				console.log('FAILED TO UPDATE CUSTOMER ACTIVE STATUS');
 				// set session error message
@@ -78,6 +129,33 @@ module.exports = (app, db, passport) => {
 			});
 		}
 	});
+
+    // route for arrived at table
+	app.post('/customer/arrived', (req, res) => {
+		if (!req.user) {
+			res.redirect('/signin');
+			// display message saying that form submission failed, plz sign in
+		}
+		else {
+			db.CustTable.update( { 
+				active: req.body.active, 
+				arrived_table: req.body.arrived_table 
+			} , {
+				where: {
+					id: req.body.id
+				}
+			}).then(result => {
+				console.log('CUSTOMER HAS BEEN SEATED AT TABLE');
+				res.send('CUSTOMER HAS BEEN SEATED AT TABLE');
+			}).catch(err => {
+				console.log('FAILED TO UPDATE CUSTOMER TABLE STATUS');
+				// set session error message
+				res.redirect('/');
+			});
+		}
+	});
+
+
 };
 
 
--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -2,14 +2,42 @@ $(document).ready(function(){
 	// activates collapsible side nav
 	$(".button-collapse").sideNav();
 	
-    // functionality for posting eat-state
-	$(document).on('click', '.cancel-btn', function () {
-		//console.log($(this).attr('data-active'));
 
-		// let thisBurger = $(this).attr('value');
-		// // posts it to the surver
+	// functionality for alerted_sms
+	$(document).on('click', '.sms-btn', function () {
+		
+		$.post('/customer/alerted', {
+			alerted_sms: true, 
+			id: $(this).attr('data-id')
+		}).done(function(data) {
+		 	console.log(data);
+		
+		}).catch(function(err) {
+			console.log(err);
+		});
+	});
+
+    // functionality for cancelling customer
+	$(document).on('click', '.cancel-btn', function () {
+		
 		$.post('/customer/cancel', {
 			active: false, 
+			id: $(this).attr('data-id')
+		}).done(function(data) {
+		 	console.log(data);
+		
+		}).catch(function(err) {
+			console.log(err);
+		});
+	});
+
+
+	// functionality for arrived table
+	$(document).on('click', '.arrived-btn', function () {
+		
+		$.post('/customer/arrived', {
+			arrived_table: true, 
+			active: false,
 			id: $(this).attr('data-id')
 		}).done(function(data) {
 		 	console.log(data);
